b842ab553e9b0b40211cf479350e226f805b7033
Thiago Bignotto <bignotto@gmail.com>
2020-04-27 02:22:47 -0300

    project start

---
+++ src/components/AppTitle/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css";
+
+export default function AppTitle() {
+  return (
+    <div className="title-container">
+      <div className="title-text">Find The Sequence Game</div>
+      <div className="text">
+        <p>Click the numbers in correct order as fast as you can!</p>
+      </div>
+    </div>
+  );
+}
