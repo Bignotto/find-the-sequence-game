27abb827b92d5023497c78fd11fa5dee26d3f847
Thiago Bignotto <bignotto@gmail.com>
2020-05-03 04:00:04 -0300

    Timer

--- src/components/Buttons/index.js
+++ src/components/Buttons/index.js
@@ -1,11 +1,28 @@
 import React from "react";
+
+import Game from "../../game/Game";
+
 import "./style.css";
 
 export default function Buttons() {
+  function handleRestart() {
+    console.log("START");
+    Game.start();
+  }
+
+  function handleRanking() {
+    console.log("S T O P");
+    Game.stop();
+  }
+
   return (
     <div className="button-container">
-      <button className="game-button">Restart</button>
-      <button className="game-button">Ranking</button>
+      <button className="game-button" onClick={handleRestart}>
+        Restart
+      </button>
+      <button className="game-button" onClick={handleRanking}>
+        Ranking
+      </button>
       <button className="game-button">Share</button>
     </div>
   );
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,35 +1,92 @@
-import React from "react";
-
-import Game from "../../game/Game";
+import React, { useEffect, useState } from "react";
 
 import "./index.css";
 
 const SIZE = 5;
 
-export default function AppTitle() {
-  let game = Game.newGame(SIZE);
+export default function GameGrid() {
+  const [time, setTime] = useState(0);
+  const [startTime, setStartTime] = useState(0);
+
+  let timer;
+
+  function newGame(size) {
+    let numbers = [];
+    let numberObj;
+
+    const max = size * size;
+
+    for (let i = 0; i < max; i++) {
+      numbers.push(i + 1);
+    }
+    return numbers;
+  }
+
+  useEffect(() => {
+    console.log("time changed", time);
+  }, []);
+
+  function start() {
+    setStartTime(Date.now());
+    timer = setInterval(update(), 1000);
+  }
+
+  function stop() {
+    clearInterval(timer);
+    setTime(0);
+  }
+
+  function update() {
+    const delta = Date.now() - startTime;
+    setTime(time + delta);
+    console.log(time);
+  }
 
   function handleClick(texto) {
     alert(texto);
   }
 
+  function handleRestart() {
+    console.log("START");
+    start();
+  }
+
+  function handleRanking() {
+    console.log("S T O P");
+    stop();
+  }
+
   return (
-    <div className="game-container">
-      <ul>
-        {game.map((num) => (
-          <li key={num}>
-            <div className="game-cell">
-              <button
-                type="button"
-                className="button-text"
-                onClick={() => handleClick(`clicou no ${num}`)}
-              >
-                {num}
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <div className="time-container">
+        <p>{time}</p>
+      </div>
+      <div className="game-container">
+        <ul>
+          {newGame(SIZE).map((num) => (
+            <li key={num}>
+              <div className="game-cell">
+                <button
+                  type="button"
+                  className="button-text"
+                  onClick={() => handleClick(`clicou no ${num}`)}
+                >
+                  {num}
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="button-container">
+        <button className="game-button" onClick={handleRestart}>
+          Restart
+        </button>
+        <button className="game-button" onClick={handleRanking}>
+          Ranking
+        </button>
+        <button className="game-button">Share</button>
+      </div>
+    </>
   );
 }
