2f8639379fc96730050b1ac578c2a83642297b60
Thiago Bignotto <bignotto@gmail.com>
2020-05-04 02:07:02 -0300

    wip: stop, restart, new game

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import AppTitle from "./components/AppTitle";
-import Time from "./components/Time";
 import Game from "./components/Game";
-import Buttons from "./components/Buttons";
 
 import "./global.css";
 
--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -4,18 +4,28 @@ import "./index.css";
 
 const SIZE = 5;
 
+const leftPad = (width, n) => {
+  if ((n + "").length > width) {
+    return n;
+  }
+  const padding = new Array(width).join("0");
+  return (padding + n).slice(-width);
+};
+
 export default class GameGrid extends React.Component {
   constructor(props) {
     super(props);
-    this.updateDate = this.updateDate.bind(this);
+    this.updateTime = this.updateTime.bind(this);
 
     this.state = {
       timeElapsed: 0,
+      isRunning: false,
+      gameNumbers: this.newGame(SIZE),
       date: Date.now(),
     };
   }
 
-  updateDate() {
+  updateTime() {
     const delta = Date.now() - this.state.date;
     this.setState({
       timeElapsed: this.state.timeElapsed + delta,
@@ -23,41 +33,88 @@ export default class GameGrid extends React.Component {
     });
   }
 
-  handleRestart() {
-    console.log("START");
-    this.interval = setInterval(this.updateDate, 1000);
+  getUnits(timeElapsed) {
+    const seconds = timeElapsed / 1000;
+    return {
+      min: Math.floor(seconds / 60).toString(),
+      sec: Math.floor(seconds % 60).toString(),
+      msec: (seconds % 1).toFixed(3).substring(2),
+    };
   }
 
-  handleRanking() {
+  stopGame() {
     console.log("S T O P");
     clearInterval(this.interval);
+    this.interval = null;
   }
 
-  handleClick(message) {
-    alert(message);
+  handleClick() {
+    console.log("Click!");
+
+    //TODO: handle game states
+    //stop button stops the clock
+    //restart button generate new numbers
+    //check if elapsed time is zero to decide if new numbers are needed
+    if (!this.interval) {
+      console.log("GameStart!");
+      this.setState({
+        timeElapsed: 0,
+        date: Date.now(),
+      });
+      this.interval = setInterval(this.updateTime, 10);
+      return;
+    }
   }
 
   newGame(size) {
-    let numbers = [];
+    let numbers1 = [];
+    let numbers2 = [];
     let numberObj;
 
     const max = size * size;
 
     for (let i = 0; i < max; i++) {
-      numbers.push(i + 1);
+      numbers1.push(1 + i);
+      numbers2.push(25 + i);
     }
-    return numbers;
+    return {
+      first: this.shuffle(numbers1),
+      second: this.shuffle(numbers2),
+    };
+  }
+
+  shuffle(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
   }
 
   render() {
+    const units = this.getUnits(this.state.timeElapsed);
     return (
       <>
         <div className="time-container">
-          <p>{this.state.timeElapsed}</p>
+          <p>
+            {leftPad(2, units.min)}:{leftPad(2, units.sec)}.{units.msec}
+          </p>
         </div>
         <div className="game-container">
           <ul>
-            {this.newGame(SIZE).map((num) => (
+            {this.state.gameNumbers.first.map((num) => (
               <li key={num}>
                 <div className="game-cell">
                   <button
@@ -73,10 +130,10 @@ export default class GameGrid extends React.Component {
           </ul>
         </div>
         <div className="button-container">
-          <button className="game-button" onClick={() => this.handleRestart()}>
-            Restart
+          <button className="game-button" onClick={() => this.stopGame()}>
+            Stop
           </button>
-          <button className="game-button" onClick={() => this.handleRanking()}>
+          <button className="game-button" onClick={() => this.stopGame()}>
             Ranking
           </button>
           <button className="game-button">Share</button>
